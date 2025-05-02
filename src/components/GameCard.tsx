35ce99bb191c10cd873270414ae04552a756c692
HOTSPOT\HOTSPOT <parthabanik1204@gmail.com>
2025-05-02 14:13:08 +0600

    displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIcon from "./PlatformIcon";
+import CriticScore from "./CriticScore";
 interface Props {
   game: Game;
 }
@@ -15,9 +16,12 @@ const GameCard = ({ game }: Props) => {
       />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIcon
-          platforms={game.parent_platforms.map(({ platform }) => platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIcon
+            platforms={game.parent_platforms.map(({ platform }) => platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
