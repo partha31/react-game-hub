30eeb42b3e32f729882c1c1079b5de8046a947c2
HOTSPOT\HOTSPOT <parthabanik1204@gmail.com>
2025-05-03 11:48:08 +0600

    clean up game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,13 +11,13 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIcon
             platforms={game.parent_platforms.map(({ platform }) => platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
